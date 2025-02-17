6bbbfad5d8f1822ba521fd74ec1cee58cb20d2b8
GIIO345 <natsarashviligiorgi7@gmail.com>
2025-02-17 15:24:41 +0400

    commit

---
+++ level03/classwork03/classwork03.py
@@ -0,0 +1,42 @@
+#  მომხმარებელს შემოატანინეთ სამი რიცხვი და გამოიტანეთ ამ რიცხვების ჯამი
+number1=input("enter number")
+number2=input("enter number2")
+number3=input("enter number3")
+print(int(number1)+int(number2)+int(number3))
+#შემოატანინეთ მომხმარებელს სახელი და შეინახეთ მისი სახელი ცვლადში
+name=input("user name")
+#შემოიტანინეთ მომხმარელს ორი რიცხვი, თუ პირველი რიცხვი მეტია მეორეზე გამოიტანეთ True ( boolean ) თუ არადა გამოიტანეთ False ( boolean )
+ricxvi1=input("number 1")
+ricxvi2=input("number 2")
+print(ricxvi1>ricxvi2)
+print(ricxvi1<ricxvi2)
+
+#მომხმარებელს შემოვატანინოთ სიტყვა და n რიცხვი, შემდეგ გამოვიტანოთ ეს სიტყვა n-ჯერ
+word=input("word")
+times=input("number")
+print(word*int(times))
+
+#შემოაყვანინეთ მომხმარებელს ორი რიცხვი, და გამოპრინტეთ ამ ორი რიცხვის გაერთიანებულ ვერსისას დამატებული 1, მაგ თუ ჯერ შემოვიდა ათი შემდეგ 11, თქვენი მიზანია გამოიტანოთ 1012, ვინაიდან 10 გაერთანებული 11 არის 1011 და 1011 + 1 არის 1012
+num1=input("num")
+num2=input("num2")
+print(int(num1+num2)+1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#vebsaitze sheni saxelis da gvaris shenaxvas
+
+
+
