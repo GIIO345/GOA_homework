15f6d4efaa811204b01772c05bb5af7be31b24de
GIIO345 <natsarashviligiorgi7@gmail.com>
2025-03-03 15:58:22 +0400

    commit

---
+++ level07/homework/homework.py
@@ -0,0 +1,14 @@
+#1) შექმენით პროგრამა, რომელშიც მომხმარებელი შემოიტანს რიცხვს, თქვენ უნდა გამოიტანოთ ამ რიცხვის ყველა გამყოფი (ის რიცხვები რომლებზეც გაყოფისას მომხმარებლის მიერ შემოტანილ რიცხვზე ნაშთი 0-ია)
+number=int(input("enter a number"))
+for i in ( 1,number):
+    if number%i==0:
+        print(i)
+#2) ახსენით რა განსხვავებაა While და for loop-ებს შორის
+"""""
+for loopshi mocemuli gvaq sami nawili sadac gansxvavebuli moqmedebebi xdeba tavidan i=0 mnishvnelobas idzens da es marto ertxel xdeba shemdeg sruldeba moqmdebeba rom shemowmdes mititebuli ricxvze naklebia tu ara,zrdadi nulidan dawyebuli ricxvi sheedreba shercheul ricxvs romelic range() weria range aqvs dasawyisi da dasasruli,for loopshi iteracia yovel i-ze xorcieldeba mxolod ertxel iqamde sanam pasuxi false ar iqneba
+xolo while loopshi chven iteaciistvis xerxemlis awyoba gvwirdeba da imis mititeba rom true shemqneli moqmedeba unda gaizardos iqamde sanam false ar miagwevs iteracias rac exeba,ase tu ar moxdeba usasrulo loopi sheiqmneba,mtavari gansxvaveba isaa rom for loopi ukve awyobilia da argumentis damateba swirdeba xolo while loops mteli sxeulis 
+"""""
+#4) რა არის ამ გამოსახულების პასუხი:
+#(1 and 0) or (1 and 1) and (0 or 0)
+# 0 or 1=1 
+#1 and 0=0
