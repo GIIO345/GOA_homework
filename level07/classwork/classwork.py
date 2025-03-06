1f4ddff5b7ac4816fa98daf4e24cd0a6eb4eea97
GIIO345 <natsarashviligiorgi7@gmail.com>
2025-03-06 17:21:24 +0400

    Update classwork.py

---
+++ level07/classwork/classwork.py
@@ -0,0 +1,24 @@
+word=input("enter a word")
+num=int(input("enter a number"))
+print(word[num])
+
+for i in range(100,0,-1):
+    print(i)
+for i in range(1,100):
+    if i%2==0:
+        print(i)
+for i in range(250,500,10):
+    print(i)
+sityva=input("enter a word")
+for i in sityva:
+    if i=="_":
+        print("True")
+        break
+else:
+    print("False")
+
+number=int(input("enter a number"))
+if number%2==0:
+    print("Yes")
+else:
+    print("No")
