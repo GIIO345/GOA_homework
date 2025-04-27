8256880206cd841ec575ba54c4fc291ca85cb837
GIIO345 <natsarashviligiorgi7@gmail.com>
2025-04-27 23:53:39 +0400

    git

--- level21/homework/homework.py
+++ level21/homework/homework.py
@@ -1,1 +1,27 @@
 
+#1 
+def check_alive(health):
+    return health>0
+#2
+def repeat_str(repeat, string):
+    p=""
+    p=string*repeat
+    return p
+#3
+def cookie(x):
+    if type(x)==str:
+        return "Who ate the last cookie? It was Zach!"
+    if  type(x)==int or type(x)==float:
+        return "Who ate the last cookie? It was Monica!"
+    else:
+        return "Who ate the last cookie? It was the dog!"
+
+#4
+def century(year):
+    p=(year+99)//100
+    return p
+#5
+def find_average(nums):
+    all=sum(nums)
+    res=all/len(nums)
+    return res        
