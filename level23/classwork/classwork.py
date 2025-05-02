cc6981e85ce2d5e10837210cada4900ba6a84b62
GIIO345 <natsarashviligiorgi7@gmail.com>
2025-05-02 14:18:49 +0400

    git

--- level23/classwork/classwork.py
+++ level23/classwork/classwork.py
@@ -1,6 +1,6 @@
 #შემოატანინეთ მომხმარებელს თავისი ასაკი და შეამოწმეთ, თუ ის 10-წლისაა ან უფრო პატარა, დაპრინტეთ "Kid", თუ 10-ზე მეტი და 18-ზე ნაკლები გამოიტანეთ "teenager", თუ 18-ზე მეტია და 30-ზე ნაკლები "adult", სხვა შემთხვევაში "unc"
 
-age=if(input("enter your age: "))
+age=(input("enter your age: "))
 if age <=10:
     print("kid")
 elif age >10 and age <18:
@@ -27,7 +27,15 @@ def count_positives_sum_negatives(arr):
     return [pss, neg]
 
 
-
+#kata 2
+def is_opposite(s1,s2):
+    if s1=="" and s2=="":
+            return False
+    for i in s1 :
+        for q in s2:
+            
+            return i!=q
+        
 
 #kata 3
 def monkey_count(n):
