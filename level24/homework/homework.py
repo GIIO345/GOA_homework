e2f3473c67613988f30336f926c5c1f0a1c30d69
GIIO345 <natsarashviligiorgi7@gmail.com>
2025-05-04 20:07:09 +0400

    git

--- level24/homework/homework.py
+++ level24/homework/homework.py
@@ -5,4 +5,17 @@ def digitize(n):
         j.append(int(i))
     res=j[::-1]
     return res
-    
+
+#kata 2
+def to_alternating_case(string):
+    word=""
+    for i in string:
+        if i.islower():
+            word+=i.upper()
+        elif i.isupper():
+            word+=i.lower()
+        else:
+            word+=i
+    return word
+
+#kata3
