80dca473bdb8ce83795b4845c17f14ac2c702083
GIIO345 <natsarashviligiorgi7@gmail.com>
2025-05-14 00:29:33 +0400

    git

--- level26/homework/homework.py
+++ level26/homework/homework.py
@@ -1,4 +1,4 @@
-""""
+
 შექმენით ფუნქცია, რომელიც მიიღებს მომხმარებლისგან არგუმენტს და მისი type-ის სესაბამისად გამოიტანეთ სიტყვები:
 Str - "Literature"
 Int/Float - "Math"
@@ -15,8 +15,38 @@ def typer(a):
 
     
 შექმენით ფუნქცია რომელიც არგუმენტად მიიღებს ლისტს და გამოიტანეთ ის data type რომელიც ყველაზე ხშირად გვხვდება ამ ლისტსში.
-"""""
-def listf(a):
-    return max(type()) in a
-print(listf(["a", "a", "a", 1 > 0, 0 > 1, 1 > 8, 1, 2, 0.3, 2.3, 33, 3, 3]))
 
+def litf(a):
+    strf=0
+    intf=0
+    floatf=0
+    boolf=0
+    for i in a:
+        if type(i)==str:
+            strf+=1
+        elif type(i)==int:
+            intf+=1
+        elif type(i)==float:
+            floatf+=1
+        else:
+            boolf+=1
+    return max("string"+str(strf),"int"+str(intf),"float"+str(floatf),"bool"+str(boolf))
+print(litf([True, False, "hello", "world","i", "python", 42, 100, -7, 3.14, 2.718, 0.001]))
+
+
+
+#შექმენით ფუნქცია, რომელსაც გადაეცემა list, თუ ამ ლისტში არის ყველა integer და ერთი string ან სხვა ნებისმიერი data type, თქვენ უნდა დააბრუნოით იგივე list #განსხვავებული data type-ის გარეშე.
+def func(a):
+    lf = []
+    for i in a:
+        if type(i) in (bool, str, float):
+            lf.append(int(i))
+    return lf
+print(func([True, False, "1", "1","1", "1", 42, 100, -7, 3.14, 2.718, 0.001]))
+
+
+
+#შექმენით ფუნქცია სადაც მომხარებელი შეიყვანს რაიმე მათემატიკურ ოპერაციას და თქვენ უნდა დააბრუნოთ საბოლოო პასუხის type.
+def meth(a):
+    return type(a)
+  
