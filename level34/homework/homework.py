3872171c74917648ffca1da1013397f999f05d7c
GIIO345 <natsarashviligiorgi7@gmail.com>
2025-06-18 17:38:51 +0400

    git

--- level34/homework/homework.py
+++ level34/homework/homework.py
@@ -4,5 +4,22 @@
 #          number+=str(int(i)**2)
 #      return int(number)
 
+#2 ვერ გავიგე 
+
+# #3
+# def dont_give_me_five(start,end):
+#     summ=0
+#     for i in range(start,end+1):
+#         if "5" not in str(i):
+#             summ+=1
+#     return summ
 
 
+#4 არ იძებნება გუგლში ამ კოდ ვარსის ლინკი
+
+# #5def remove_smallest(numbers):
+#     if not numbers:
+#         return []
+#     nums=numbers.copy()
+#     nums.remove(min(nums))
+#     return nums
