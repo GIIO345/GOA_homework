d8901a01fa6bd290eaf06dccba6669899c02a391
GIIO345 <natsarashviligiorgi7@gmail.com>
2025-06-25 16:28:51 +0400

    git

---
+++ level37/classwork/classwork.py
@@ -0,0 +1,15 @@
+# def quarter_of(month):
+#     return (month-1)//3+1
+
+
+
+# def find_average(numbers):
+#     if not numbers:
+#         return 0
+#     summe=sum(numbers)
+#     lene=len(numbers)
+#     return summe/lene
+
+# def remove_exclamation_marks(s):
+#     return s.replace("!","")
+
---
+++ level37/homework/homework.py
@@ -0,0 +1,26 @@
+# def accum(st):
+#     result = []
+#     for i in range(len(st)):
+#         letter = st[i]
+#         part = letter.upper() + letter.lower() * i
+#         result.append(part)
+#     return "-".join(result)
+
+
+# def descending_order(num):
+#     list=[]
+#     for i in str(num):
+#         list.append(i)
+#     return int("".join(sorted(list, reverse=True)))
+
+# def is_square(n):
+#     if n<0:
+#         return False
+#     return (n**0.5)**2==n
+
+
+# ef get_count(sentence):
+#     a="aeiou"
+#     return sum(1 for i in sentence if i in a)
+
+
